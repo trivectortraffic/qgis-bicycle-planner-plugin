a716c4414cf79d4557e4eba76b7760521873a037
Joakim Fors <joakim.fors@trivector.se>
2021-06-08 08:12:50 +0200

    feat: gather network fids by using a custom strategy
    Use a secondary custom network cost strategy to save the fid of the
    underlygin road network. This allows us to map the resulting graph
    edges back onto the network without having to use geometry ops.
    
    The current solution saves the integer ids as a comma separated
    string. The plan is to not create a layer and instead gather
    the results in a (numpy) table for easier processing.

--- bicycle_planner/ops.py
+++ bicycle_planner/ops.py
@@ -5,6 +5,7 @@ from qgis.analysis import (
     QgsVectorLayerDirector,
     QgsGraphBuilder,
     QgsGraphAnalyzer,
+    QgsNetworkStrategy,
 )
 from qgis.core import (
     edit,
@@ -18,6 +19,23 @@ from qgis.core import (
 from PyQt5.QtCore import QVariant
 
 from .utils import timing
+
+
+class SaveFidStrategy(QgsNetworkStrategy):
+    """
+    Strategy class to save the underlyging network link feature ids. The QGIS
+    graph builder does not save information about the network used to build
+    the routing graph. This class is a hack to enable mapping route_points paths back
+    onto the network by querying the cost of each edge. The edge object returns
+    the fid of the network.
+    """
+
+    def cost(self, distance: float, feature: QgsFeature) -> int:
+        try:
+            return int(feature.id())
+        except ValueError:
+            # TODO: log trace, make sure to do it in a thread safe way
+            return -1
 
 
 @timing()
@@ -43,6 +61,7 @@ def shortest_path(
     """
 
     DISTANCE_FIELD = 'bp_distance'
+    NETWORK_ID_FIELD = 'network_fids'
     FROM_ID_FIELD = 'bp_from_id'
     TO_ID_FIELD = 'bp_to_id'
     FROM_TO_FIELD = 'id'
@@ -53,12 +72,12 @@ def shortest_path(
     # Add attribute fields
     with edit(output_layer):
         output_layer.addAttribute(QgsField(DISTANCE_FIELD, QVariant.Double))
+        output_layer.addAttribute(QgsField(NETWORK_ID_FIELD, QVariant.String))
         output_layer.addAttribute(QgsField(FROM_ID_FIELD, QVariant.Int))
         output_layer.addAttribute(QgsField(TO_ID_FIELD, QVariant.Int))
         output_layer.addAttribute(QgsField(FROM_TO_FIELD, QVariant.String))
 
     ## prepare graph
-    strategy = QgsNetworkDistanceStrategy()
     director = QgsVectorLayerDirector(
         source=network_layer,
         directionFieldId=-1,
@@ -67,7 +86,8 @@ def shortest_path(
         bothDirectionValue='',
         defaultDirection=QgsVectorLayerDirector.DirectionBoth,
     )
-    director.addStrategy(strategy)
+    director.addStrategy(QgsNetworkDistanceStrategy())  # 0
+    director.addStrategy(SaveFidStrategy())  # 1
     builder = QgsGraphBuilder(crs)
 
     ## prepare points
@@ -93,22 +113,22 @@ def shortest_path(
     point_id_map = dict(zip(point_ids, tied_points))
 
     n = relations_data.featureCount()
-    flows = {}
     prev_point_id = -1
     with timing('calculate connecting routes'), edit(output_layer):
         for i, feature in enumerate(relations_data.getFeatures()):
-            point_id = int(feature[origin_field])
-            near_id = int(feature[destination_field])
+            from_point_id = int(feature[origin_field])
+            to_point_id = int(feature[destination_field])
 
-            from_point = point_id_map[point_id]
-            to_point = point_id_map[near_id]
+            from_point = point_id_map[from_point_id]
+            to_point = point_id_map[to_point_id]
             # TODO: check for NullIsland point (0.0, 0.0) == not found on network
             from_vertex_id = graph.findVertex(from_point)
             to_vertex_id = graph.findVertex(to_point)
 
             # New start point => new tree
-            if point_id != prev_point_id:
-                print(f'building dijkstra tree for {point_id}')
+            if from_point_id != prev_point_id:
+                # print(f'building dijkstra tree for {from_point_id}')
+                print('.', end='')
                 (tree, cost) = QgsGraphAnalyzer.dijkstra(graph, from_vertex_id, 0)
 
             if tree[to_vertex_id] != -1 and (
@@ -116,24 +136,36 @@ def shortest_path(
             ):
                 route_cost = cost[to_vertex_id]
                 # print(route_cost)
-                route = [graph.vertex(to_vertex_id).point()]
+                route_points = [graph.vertex(to_vertex_id).point()]
                 cur_vertex_id = to_vertex_id
+                route_fids = []
                 # Iterate the graph
                 while cur_vertex_id != from_vertex_id:
-                    cur_vertex_id = graph.edge(tree[cur_vertex_id]).fromVertex()
-                    route.append(graph.vertex(cur_vertex_id).point())
-                route.reverse()
+                    cur_edge = graph.edge(tree[cur_vertex_id])
+                    route_fids.append(cur_edge.cost(1))
+                    cur_vertex_id = cur_edge.fromVertex()
+                    route_points.append(graph.vertex(cur_vertex_id).point())
+
+                # route_points.reverse()
+                route_fids = list(
+                    dict.fromkeys(route_fids)
+                )  # NOTE: requires python >= 3.7 for ordered dict FIXME: add python version check
+                route_fids.reverse()
 
                 connector = QgsFeature(output_layer.fields())
-                connector.setGeometry(QgsGeometry.fromPolylineXY(route))
+                connector.setGeometry(QgsGeometry.fromPolylineXY(route_points))
                 connector[DISTANCE_FIELD] = route_cost
-                connector[FROM_ID_FIELD] = point_id
-                connector[TO_ID_FIELD] = near_id
-                connector[FROM_TO_FIELD] = f'{point_id}-{near_id}'
+                connector[NETWORK_ID_FIELD] = ', '.join(
+                    map(str, route_fids)
+                )  # FIXME: temporary solution, skip layer creation completely and retunr dict with values
+                connector[FROM_ID_FIELD] = from_point_id
+                connector[TO_ID_FIELD] = to_point_id
+                connector[FROM_TO_FIELD] = f'{from_point_id}-{to_point_id}'
                 output_layer.addFeature(connector)
 
-            prev_point_id = point_id
+            prev_point_id = from_point_id
 
-            progress(100.0 * i / n)
+            # progress(100.0 * i / n)
+        print()
 
     return output_layer
