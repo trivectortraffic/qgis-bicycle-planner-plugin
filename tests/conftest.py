5a849e3a8dc6f64b8f7c0f8da398415d813c4f90
Joakim Fors <joakim.fors@trivector.se>
2021-10-07 13:39:18 +0200

    test: remove processing fixture as it is now included in pytest-qgis

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,13 +9,3 @@ pytest_plugins = ('pytest_qgis',)
 def datadir(pytestconfig):
     rootdir = pytestconfig.rootdir
     return os.path.join(rootdir, 'tests', 'data')
-
-
-@fixture(autouse=True, scope='session')
-def qgis_processing(qgis_app):
-    python_plugins_path = os.path.join(qgis_app.pkgDataPath(), 'python', 'plugins')
-    if python_plugins_path not in sys.path:
-        sys.path.append(python_plugins_path)
-    from processing.core.Processing import Processing
-
-    Processing.initialize()
